658708e6d83ea3f898de103d716eebfd19fb24de
tmckibben <tmckibben1138@gmail.com>
2016-02-18 21:29:12 -0600

    Initial commit
    Initial commit of app scaffolding.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cpsSuspensionApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cpsSuspensionApp
+ */
+angular.module('cpsSuspensionApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cpsSuspensionApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cpsSuspensionApp
+ */
+angular.module('cpsSuspensionApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
